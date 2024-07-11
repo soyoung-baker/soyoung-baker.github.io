d0ffb451d02b775838b68d0a91c03e338b3d1edd
soyoung-baker <zero.so.jung@gmail.com>
2024-07-11 19:57:07 +0900

    :sparkles: layout 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import { ReactNode } from 'react'
 
 import { Inter } from 'next/font/google'
 
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,7 +12,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" className={inter.className}>
-      <body>{children}</body>
+      <body className="flex flex-col w-full max-w-screen-lg mx-auto">
+        <Header />
+        <main className="grow">{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>hello</main>
+  return <section>hello</section>
 }
