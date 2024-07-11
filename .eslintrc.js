f4aa8bb3099ba822eae55a0ee83da096f47c0d3f
soyoung-baker <zero.so.jung@gmail.com>
2024-07-11 19:36:57 +0900

    :package: Next 관련 패키지 설치 및 초기 환경 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:jsx-a11y/recommended'],
+  plugins: ['jsx-a11y', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {},
+}
---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: false,
+  parser: 'typescript',
+  arrowParens: 'always',
+  importOrder: [
+    '^react',
+    '^next',
+    '<THIRD_PARTY_MODULES>',
+    '^~/api/(.*)$',
+    '^~/hooks/(.*)$',
+    '^~/components/(.*)$',
+    '^~/utils/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
